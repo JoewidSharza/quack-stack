984fce2960788bfa539760b1dc2d2288466825f9
Maxwell <developer@max-girasol.com>
2025-02-28 16:44:57 -0500

    edit bettingresults to map

--- duckBets/vite-project/src/BettingResults.jsx
+++ duckBets/vite-project/src/BettingResults.jsx
@@ -1,3 +1,29 @@
+const namesArray = [
+  { id: 1, name: "AJ Zimmerman" },
+  { id: 2, name: "Max Girasol" },
+  { id: 3, name: "Faiq Ahmad" },
+  { id: 4, name: "joewid Sharza" },
+  { id: 5, name: "CMoney." },
+  { id: 6, name: "DJ Chrispy." },
+  { id: 7, name: "Stephen Thomas" },
+  { id: 8, name: "Ani Undrakonda" },
+  { id: 9, name: "Anish Ivaturi" },
+  { id: 10, name: "Joshua St. Laurent" },
+  { id: 11, name: "Matt Cheng" },
+  { id: 12, name: "Timothy Germano" },
+  { id: 13, name: "Dev Patel" },
+  { id: 14, name: "Hilarion Wilkenson" },
+  { id: 15, name: "Lavanya Goel" },
+  { id: 16, name: "Robin Pan" },
+  { id: 17, name: "Henry Daley" },
+  { id: 18, name: "Tia Nguyen" },
+  { id: 19, name: "Ben Baiel" },
+  { id: 20, name: "Weiting Huang" },
+  { id: 21, name: "Chris Park" },
+  { id: 22, name: "Kasey Simmons" },
+  { id: 23, name: "Nate Notermann" },
+];
+
 function BettingResults() {
   return (
     <table>
@@ -8,18 +34,12 @@ function BettingResults() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Duck 1</td>
-          <td>Winner</td>
-        </tr>
-        <tr>
-          <td>Duck 2</td>
-          <td>Loss</td>
-        </tr>
-        <tr>
-          <td>Duck 3</td>
-          <td>Loss</td>
-        </tr>
+        {namesArray.map((duckName) => (
+          <tr key={duckName.id}>
+            <td>{duckName.name}</td>
+            <td>{duckName.id}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
